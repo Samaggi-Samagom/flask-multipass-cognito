de19dd9119d09a2b4404ca39abcd515f95b59744
Panawat Wong-klaew <panawat_vista@hotmail.com>
2020-07-26 22:46:03 +0700

    test logging identity

---
+++ flask_multipass_cognito/identity.py
@@ -0,0 +1,24 @@
+from flask_multipass.providers.authlib import AuthlibIdentityProvider
+from indico.core.logger import Logger
+
+
+class CognitoIdentityProvider(AuthlibIdentityProvider):
+    """Provides identity information of Amazon Cognito user using Authlib
+
+    This extends ``AuthlibIdentityProvider`` with additional capabilities provided by
+    Amazon Cognito User Pool API.
+    """
+
+    #: If the provider supports getting identity information based from
+    #: an identifier
+    supports_get = False
+
+    def __init__(self, *args, **kwargs):
+        super(CognitoIdentityProvider, self).__init__(*args, **kwargs)
+        self.aws_profile = self.settings.setdefault('aws_profile', 'default')
+        self.logger = Logger.get('cognito') 
+
+    def get_identity_from_auth(self, auth_info):
+        identityInfo = super(CognitoIdentityProvider, self).get_identity_from_auth(authinfo)
+        self.logger.info('got {} from {}'.format(identityInfo, auth_info))
+        return identityInfo
