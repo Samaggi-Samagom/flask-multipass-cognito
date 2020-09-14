8834ffee21413cb9b9d661ffe9eeeaca8a4c1f08
Panawat Wong-kleaw <panawat_vista@hotmail.com>
2020-09-14 22:16:13 +0700

    Use the new production release of Flask-Multipass
    And also bump the version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-Multipass-Cognito',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/Samaggi-Samagom/flask-multipass-cognito',
     description='A Flask Multipass Authentication and Identity providers for Amazon Cognito',
     author='Samaggi Samagom',
     author_email='cto@samaggisamagom.com',
     packages=find_packages(),
     install_requires=[
-        'Flask-Multipass[authlib]==0.3.dev5',
+        'Flask-Multipass[authlib]>=0.3',
         'boto3'
     ],
     entry_points={
